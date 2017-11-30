a0ce00ed08938483ef470b5157b1b8dc2979506d
denisden83 <denisden83@gmail.com>
2017-11-30 20:42:49 +0100

    2.3, 2.4

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@
  Зарпещено использовать встроенные методы для работы с массивами
  */
 function isAllTrue(array, fn) {
-    if (!(array instanceof Array && array.length > 0)) throw new Error('empty array');
+    if ((!(array instanceof Array)) || array.length < 1) throw new Error('empty array');
     if (typeof fn != 'function') throw new Error('fn is not a function');
     let result;
 
@@ -62,7 +62,21 @@ function isSomeTrue(array, fn) {
  Необходимо выбрасывать исключение в случаях:
  - fn не является функцией (с текстом "fn is not a function")
  */
-function returnBadArguments(fn) {
+function returnBadArguments(...arr) {
+    if (typeof arr[0] != 'function') throw new Error('fn is not a function');
+    let newArr = [];
+
+    for (let i = 1; i < arr.length; i++) {
+        try {
+            arr[0](arr[i]);
+        } catch (e) {
+            if (e) {
+                newArr.push(arguments[i]);
+            }
+        }
+    }
+
+    return newArr;
 }
 
 /*
@@ -79,7 +93,48 @@ function returnBadArguments(fn) {
  - number не является числом (с текстом "number is not a number")
  - какой-либо из аргументов div является нулем (с текстом "division by 0")
  */
-function calculator() {
+function calculator(number = 0) {
+    if (isNaN(number)) throw new Error('number is not a number');
+
+    return {
+        sum: function(...arg) {
+            let result = number;
+
+            for (let i = 0; i < arg.length; i++) {
+                result += arg[i];
+            }
+
+            return result;
+        },
+        dif: function(...arg) {
+            let result = number;
+
+            for (let i = 0; i < arg.length; i++) {
+                result -= arg[i];
+            }
+
+            return result;
+        },
+        div: function(...arg) {
+            let result = number;
+
+            for (let i = 0; i < arg.length; i++) {
+                if (arg[i] === 0) throw new Error('division by 0');
+                result /= arg[i];
+            }
+
+            return result;
+        },
+        mul: function(...arg) {
+            let result = number;
+
+            for (let i = 0; i < arg.length; i++) {
+                result *= arg[i];
+            }
+
+            return result;
+        }
+    }
 }
 
 export {
