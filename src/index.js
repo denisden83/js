d008315209bab56795f13547570c325fd36e25f8
denisden83 <denisden83@gmail.com>
2017-12-12 02:24:28 +0100

    dz 6.1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+
+'use strict';
+
 /* ДЗ 6.1 - Асинхронность и работа с сетью */
 
 /**
@@ -6,8 +9,14 @@
  * @param {number} seconds - количество секунд, через которое Promise должен быть resolved
  * @return {Promise}
  */
-function delayPromise(seconds) {
-}
+let delayPromise = seconds => {
+    return new Promise((resolve, reject) => {
+        setTimeout(function () {
+            if (false) return reject(new Error("DEFINITELY WON'T EVER BE REJECTED"));
+            resolve();
+        }, seconds * 1000);
+    });
+};
 
 /**
  * Функция должна вернуть Promise, который должен быть разрешен массивом городов, загруженным из
@@ -17,7 +26,52 @@ function delayPromise(seconds) {
  * @return {Promise<Array<{name: String}>>}
  */
 function loadAndSortTowns() {
+    const url = 'https://raw.githubusercontent.com/smelukov/citiesTest/master/cities.json';
+
+    return new Promise((resolve, reject) => {
+        if (false) return reject(new Error("DEFINITELY WON'T EVER BE REJECTED"));
+        let xhr = new XMLHttpRequest();
+
+        xhr.open('GET', url);
+        xhr.send();
+        xhr.addEventListener('load', () => {
+            let gotArr = JSON.parse(xhr.responseText);
+
+            // ===============  ??????? почему так не сортирует ??????
+            // gotArr.sort((a, b) => a.name > b.name);
+            // ===============  ??????? почему так не сортирует ??????
+
+            // let arr2 = [];
+
+            let arr2 = gotArr.map(obj => obj.name);
+            // gotArr.forEach(obj => arr2.push(obj.name));
+
+            // for (let obj of gotArr) {
+            //     arr2.push(obj.name);
+            // }
+
+            // for (let i = 0; i < gotArr.length; i++) {
+            //     arr2.push(gotArr[i].name);
+            // }
+            let arr3 = arr2.sort();
+            // let finalArr = [];
+
+            let finalArr = arr3.map(elem => ({ name: elem }));
+            // arr3.forEach(elem => finalArr.push({ name: elem }));
+
+            // for (let elem of arr3) {
+            //     finalArr.push({ name: elem });
+            // }
+
+            // for (let k = 0; k < arr3.length; k++) {
+            //     finalArr.push({ name: arr3[k] });
+            // }
+
+            resolve(finalArr);
+        });
+    });
 }
+loadAndSortTowns();
 
 export {
     delayPromise,
